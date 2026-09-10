9bc547133decf7e17b0733a264256af4cc700d02
kidistm03 <kidistmeseret1@gmail.com>
2026-09-10 22:35:58 +0300

    import all pages to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Browse from './pages/Browse'
+import MovieDetail from './pages/MovieDetail'
+import SearchResults from './pages/SearchResults'
+import Watchlist from './pages/Watchlist'
+import NotFound from './pages/NotFound'
+
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-950 text-white">
-      <h1 className="text-4xl font-bold text-center pt-10">
-        Cinema App
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
