6ed64599d95e515df0e82fe0269232573c579b00
kidistm03 <kidistmeseret1@gmail.com>
2026-09-11 01:46:07 +0300

    create movie grid

---
+++ src/components/movie/MovieGrid.jsx
@@ -0,0 +1,16 @@
+import MovieCard from './MovieCard'
+
+function MovieGrid({ movies }) {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default MovieGrid
