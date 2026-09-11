a64faf2bb4147bb0de32ceb5d28c60fa98f712d3
kidistm03 <kidistmeseret1@gmail.com>
2026-09-12 00:20:26 +0300

    add similar movies

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import MovieCard from '../components/movie/MovieCard'
 
 const KEY = import.meta.env.VITE_TMDB_KEY
 const BASE_URL = 'https://api.themoviedb.org/3'
@@ -9,6 +10,7 @@ function MovieDetail() {
 
   const [movie, setMovie] = useState(null)
   const [cast, setCast] = useState([])
+  const [similarMovies, setSimilarMovies] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
@@ -50,11 +52,26 @@ function MovieDetail() {
         if (!creditsResponse.ok) {
           throw new Error('Failed to fetch cast')
         }
-
         const creditsData = await creditsResponse.json()
-
         setCast(creditsData.cast.slice(0, 10))
 
+        const similarResponse = await fetch(
+          `${BASE_URL}/movie/${id}/similar`,
+          {
+            headers: {
+              Authorization: `Bearer ${KEY}`,
+            },
+            signal: controller.signal,
+          }
+        )
+
+        if (!similarResponse.ok) {
+          throw new Error('Failed to fetch similar movies')
+        }
+
+        const similarData = await similarResponse.json()
+
+        setSimilarMovies(similarData.results)
       } catch (error) {
         if (error.name !== 'AbortError') {
           setError(error.message)
@@ -173,7 +190,20 @@ function MovieDetail() {
                 </div>
               ))}
             </div>
+            <h2 className="text-2xl font-bold mt-10 mb-4">
+              Similar Movies
+            </h2>
 
+            <div className="flex gap-4 overflow-x-auto pb-6">
+              {similarMovies.slice(0, 10).map((movie) => (
+                <div
+                  key={movie.id}
+                  className="min-w-[160px] sm:min-w-[190px] md:min-w-[210px]"
+                >
+                  <MovieCard movie={movie} />
+                </div>
+              ))}
+            </div>
           </div>
 
         </div>
